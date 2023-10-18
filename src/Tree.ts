7aeed1689063a79c7b6815cc50e400a069fdf8b9
Scott <sgarland1962@yahoo.com>
2023-10-18 18:17:11 -0500

    Add branches to Tree

--- src/Tree.ts
+++ src/Tree.ts
@@ -1,8 +1,46 @@
 export default class Tree {
     size: number;
     color: string;
-    constructor(size, color) {
+    strain: string;
+    branches: Branch[];
+
+    constructor(size: number, color: string, strain: string) {
         this.size = size;
         this.color = color;
+        this.strain = strain;
+        this.branches = [];
+    }
+
+    grow(food: number, light: number) {
+        if (food + light >= 3) {
+            this.branches.push(new Branch());
+            this.branches.forEach((branch) => {
+                branch.grow(food, light);
+            });
+        }    
+        
+    }
+
+}
+class Branch {
+    size: number;
+    buds: number;
+    branches: Branch[];
+    constructor() {
+        this.size = 1;
+        this.buds = 0;
+        this.branches = [];
+    }
+    grow(food: number, light: number) {
+        if (food + light >= 3) {
+            this.branches.push(new Branch());
+            this.branches.forEach((branch) => {
+                branch.grow(food/2, light/2);
+            });
+            this.size++;
+        }    
+        if (food + light >= 10) {
+            this.buds++;
+        } 
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import express from "express";
 import Tree from "./Tree";
 
 const app = express();
+app.listen(3000, () => {
+    console.log("gotcha")
+})
+const x: Tree = new Tree(10, "green", "AK-47");
+const y: Tree = new Tree(9, "green", "BBG");
+const z: Tree = new Tree(11, "green", "JJ")
+console.log(x, y, z);
 
-const x: Tree = new Tree(10, "green");
-const y: Tree = new Tree(9, "green");
-console.log(x, y);
+app.get("/", (request, response) => {
+    x.grow(3, 3);
+    y.grow(7,4);
+    z.grow(2500, 2500);
+    response.send([x, y, z]);
+})
